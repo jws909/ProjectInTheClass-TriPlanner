390a3e0399e382ef2e7c5dd2bb8f9b5177069f90
hwisu jang <jws090909@gmail.com>
2018-05-17 14:59:56 +0900

    Initial Commit

---
+++ TriPlanner/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TriPlanner
+//
+//  Created by hyuapp1 on 2018. 5. 17..
+//  Copyright © 2018년 hyuapp1. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
